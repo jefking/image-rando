a317ba29f1009bc63cad700c15d357dc0f574c5b
jef king <jbdking@pm.me>
2026-01-20 20:29:13 -0800

    fixing

--- target/debug/deps/image_rando-ec7f0c3f36a1f3ce.d
+++
@@ -1,5 +0,0 @@
-/home/jef/git/image-rando/target/debug/deps/image_rando-ec7f0c3f36a1f3ce.d: src/main.rs
-
-/home/jef/git/image-rando/target/debug/deps/image_rando-ec7f0c3f36a1f3ce: src/main.rs
-
-src/main.rs:
--- target/debug/image-rando.d
+++
@@ -1,1 +0,0 @@
-/home/jef/git/image-rando/target/debug/image-rando: /home/jef/git/image-rando/src/main.rs
